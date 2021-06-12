75fb5c58577933bd43b14d5568104f9be0f23e29
Pavs315 <boddu.pavani@research.iiit.ac.in>
2021-06-13 03:34:52 +0530

    run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 . ./path.sh || exit 1
 . ./cmd.sh || exit 1
 nj=1       # number of parallel jobs - 1 is perfect for such a small dataset
@@ -11,14 +12,7 @@ rm -rf exp mfcc data/train/spk2utt data/train/cmvn.scp data/train/feats.scp data
 echo
 echo "===== PREPARING ACOUSTIC DATA ====="
 echo
-# Needs to be prepared by hand (or using self written scripts):
-#
-# spk2gender  [<speaker-id> <gender>]
-# wav.scp     [<uterranceID> <full_path_to_audio_file>]
-# text        [<uterranceID> <text_transcription>]
-# utt2spk     [<uterranceID> <speakerID>]
-# corpus.txt  [<text_transcription>]
-# Making spk2utt files
+
 utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
 echo
@@ -26,9 +20,7 @@ echo "===== FEATURES EXTRACTION ====="
 echo
 # Making feats.scp files
 mfccdir=mfcc
-# Uncomment and modify arguments in scripts below if you have any problems with data sorting
-# utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
-# utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
+
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train exp/make_mfcc/train $mfccdir
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/test exp/make_mfcc/test $mfccdir
 # Making cmvn.scp files
@@ -37,13 +29,7 @@ steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
 echo
 echo "===== PREPARING LANGUAGE DATA ====="
 echo
-# Needs to be prepared by hand (or using self written scripts):
-#
-# lexicon.txt           [<word> <phone 1> <phone 2> ...]
-# nonsilence_phones.txt [<phone>]
-# silence_phones.txt    [<phone>]
-# optional_silence.txt  [<phone>]
-# Preparing language data
+
 utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
 echo
 echo "===== LANGUAGE MODEL CREATION ====="
